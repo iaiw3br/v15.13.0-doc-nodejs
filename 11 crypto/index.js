3323e0f7b488770e6d33f79d7cd430bb8ba41c64
Артем Иванов <iaiw3br@gmail.com>
2021-04-16 18:59:53 +0300

    feat: crypto (in work)

---
+++ 11 crypto/index.js
@@ -0,0 +1,17 @@
+const crypto = require('crypto');
+/*
+* Предоставляет криптографические функции включающие в себя установку обёрток для OpenSSL hash, HMAC,
+* cipher, decipher, sign и защищенные функции
+* */
+// const secretWord = 'top_secret';
+// const hash = crypto.createHmac('sha256', secretWord)
+//     .update('Hi')
+//     .digest('hex');
+// console.log(hash)
+// 644bf3e67e05b3848729c3c20ae2d2d8a24be19cb6144406fb6dcfbc937319c1
+
+
+/*
+* Certificate
+* SPKAC сертификат включает механизм запроса на подпись сертификата
+* */
