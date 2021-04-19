fc46874a8ae81e7b36442668ec7c66bbd66c3a81
Артем Иванов <iaiw3br@gmail.com>
2021-04-19 22:39:16 +0300

    feat: crypto

--- 11 crypto/index.js
+++ 11 crypto/index.js
@@ -242,4 +242,225 @@ const assert = require('assert');
 
 /*
 * Class: Hmac
+* класс для создания криптографических HMAC. Может использоваться двумя способами:
+* 1. Потоки
+* 2. hmac.update(), hmac.digest()
+* */
+// const hmac = crypto.createHmac('sha256', 'a secret');
+//
+// hmac.on('readable', () => {
+//     // Only one element is going to be produced by the
+//     // hash stream.
+//     const data = hmac.read();
+//     if (data) {
+//         console.log(data.toString('hex'));
+//         // Prints:
+//         //   7fd04df92f636fd450bc841c9418e5825c17f33ad9c87c518115a45971f7f77e
+//     }
+// });
+//
+// hmac.write('some data to hash');
+// hmac.end();
+
+
+// stream
+// const hmac = crypto.createHmac('sha256', 'a secret');
+// const input = fs.createReadStream('test.js');
+// input.pipe(hmac).pipe(process.stdout);
+
+
+/*
+* hmac.digest([encoding])
+* Вычисляет HMAC всех данных, переданных с помощью hmac.update()
+* */
+
+
+/*
+* hmac.update(data[, inputEncoding])
+* Обновляет HMAC по предоставленным данным.
+* */
+
+
+/*
+* Class: KeyObject
+* Для предоставления симметричного или асимметричного ключа и каждый вид ключа предоставляет разные функции.
+* Следует использовать API KeyObject вместо передачи ключей в виде строк или буферов.
+* */
+
+
+/*
+* Static method: KeyObject.from(key)
+* Преобразует CryptoKey в KeyObject
+* */
+
+// (async () => {
+//     const key = await crypto.webcrypto.subtle.generateKey({
+//         name: 'HMAC',
+//         hash: 'SHA-256',
+//         length: 256
+//     }, true, ['sign', 'verify']);
+//
+//     const keyObject = crypto.KeyObject.from(key);
+//     console.log(keyObject.symmetricKeySize); // 32
+// })()
+
+
+/*
+* keyObject.asymmetricKeyDetails
+* Это свойство включает только асимметрический ключ. В зависимости от типа ключа, объект содержит информацию
+* об ключе. Никакая информация, полученная с помощью этого свойства, не может использоваться для однозначной
+* идентификации ключа или для нарушения безопасности ключа.
+* */
+
+
+/*
+* keyObject.asymmetricKeyType
+* Для асимметричных ключей, это свойство представляет тип ключа. Варианты:
+* 'rsa' (OID 1.2.840.113549.1.1.1)
+* 'rsa-pss' (OID 1.2.840.113549.1.1.10)
+* 'dsa' (OID 1.2.840.10040.4.1)
+* 'ec' (OID 1.2.840.10045.2.1)
+* 'x25519' (OID 1.3.101.110)
+* 'x448' (OID 1.3.101.111)
+* 'ed25519' (OID 1.3.101.112)
+* 'ed448' (OID 1.3.101.113)
+* 'dh' (OID 1.2.840.113549.1.3.1)
+* */
+
+
+/*
+* keyObject.export([options])
+* Для симметричных ключей
+* */
+
+
+/*
+* keyObject.symmetricKeySize
+* Для секретного ключа, это свойство представляет размер ключа в байтах. Вернёт undefined если это
+* асимметричный ключ.
+* */
+
+
+/*
+* keyObject.type
+* В зависимости от типа KeyObject это свойство является либо секретным для симметричных ключей, общедоступным
+* для асимметричных ключей и частным для асимметричных частных ключей.
+* */
+
+
+/*
+* Class: Sign
+* Класс для генерации подписей. Может использоваться в двух случаях:
+* 1. Потоки
+* 2. sign.update(), sign.sign()
+* */
+
+// Stream
+// const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+//     namedCurve: 'sect239k1'
+// });
+//
+// const sign = crypto.createSign('SHA256');
+// sign.write('some data to sign');
+// sign.end();
+// const signature = sign.sign(privateKey, 'hex');
+//
+// const verify = crypto.createVerify('SHA256');
+// verify.write('some data to sign');
+// verify.end();
+// console.log(verify.verify(publicKey, signature, 'hex')); //true
+
+
+// sign.update(), sign.sign()
+// const { privateKey, publicKey } = crypto.generateKeyPairSync('rsa', {
+//     modulusLength: 2048,
+// });
+//
+// const sign = crypto.createSign('SHA256');
+// sign.update('some data to sign');
+// sign.end();
+// const signature = sign.sign(privateKey);
+//
+// const verify = crypto.createVerify('SHA256');
+// verify.update('some data to sign');
+// verify.end();
+// console.log(verify.verify(publicKey, signature)); // true
+
+
+/*
+* Class: Verify
+* Класс для проверки подписей. Может сипользоваться в двух случаях:
+* 1. Потоки
+* 2. verify.update() and verify.verify()
+* */
+
+
+/*
+* verify.update(data[, inputEncoding])
+* Обновляет содержимое Verify с указанными данными
+* */
+
+
+/*
+* verify.verify(object, signature[, signatureEncoding])
+* Проверяет предоставленные данные, используя указанный объект и подпись.
+* */
+
+
+/*
+* Class: X509Certificate
+* Инкапсулирует сертификат X509 и обеспечивает доступ только для чтения к его информации.
+* */
+// const x509 = new crypto.X509Certificate('{... pem encoded cert ...}');
+//
+// console.log(x509.subject);
+
+
+/*
+* new X509Certificate(buffer)
+* Сертификат X509 в кодировке PEM или DER.
+*
+* x509.ca
+* Будет true, если это сертификат центра сертификации (CA).
+*
+*
+* x509.checkEmail(email[, options])
+* Проверяет, соответствует ли сертификат указанному адресу электронной почты.
+*
+*
+* x509.checkHost(name[, options])
+* Проверяет, соответствует ли сертификат заданному имени хоста.
+*
+*
+* x509.checkIP(ip[, options])
+* Проверяет, соответствует ли сертификат заданному IP-адресу (IPv4 или IPv6).
+*
+*
+* x509.checkIssued(otherCert)
+* Проверяет, был ли этот сертификат выдан данным otherCert.
+*
+*
+* x509.checkPrivateKey(privateKey)
+* Проверяет, соответствует ли открытый ключ этого сертификата заданному закрытому ключу.
+*
+*
+* x509.fingerprint
+* Отпечаток SHA-1 этого сертификата.
+*
+*
+* x509.fingerprint256
+* Отпечаток этого сертификата SHA-256
+* */
+
+
+/*
+* crypto.checkPrime(candidate[, options, [callback]])
+* crypto.checkPrimeSync(candidate[, options])
+* Проверяет примитивность кандидата.
+* */
+
+
+/*
+* crypto.createCipheriv(algorithm, key, iv[, options])
+* Создает и возвращает объект Cipher с заданным алгоритмом, ключом и вектором инициализации (iv).
 * */
