725e76b1c0dd9cece21affe3eb643367d7d574f4
Артем Иванов <iaiw3br@gmail.com>
2021-04-27 22:30:55 +0300

    feat: string decoder

---
+++ 39 string decoder/index.js
@@ -0,0 +1,26 @@
+const {StringDecoder} = require('string_decoder')
+/*
+* String decoder
+* Модуль предоставляет декодирование Buffer в строку.
+* */
+// const decoder = new StringDecoder('utf8');
+// const buf = Buffer.from('hi');
+// console.log(decoder.write(buf)); // hi
+
+
+/*
+* new StringDecoder([encoding])
+* default utf8
+* */
+
+
+/*
+ * stringDecoder.end([buffer])
+ * Возвращает оставшиеся данные в виде строки.
+ *  */
+
+
+/*
+* stringDecoder.write(buffer)
+* Возвращает декодированную строку.
+* */
